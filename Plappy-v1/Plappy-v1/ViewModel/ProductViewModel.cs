fac69170bb7da4c553a371131dd0e57c0eb94f6d
yslolly <anneleen.tijtgat@outlook.com>
2021-01-16 19:52:36 +0100

    file aangepast naar MVVM-model

---
+++ Plappy-v1/Plappy-v1/ViewModel/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plappy_v1.ViewModel
+{
+    class ProductViewModel
+    {
+        // hier alle methoden implementeren
+    }
+}
