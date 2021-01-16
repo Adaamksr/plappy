50d3a7ae4d5281ccc34f2570caca95fc1757a1e8
Adaamksr <adamkasri@gmail.com>
2021-01-16 22:16:56 +0100

    Maken van basis design

--- Plappy-v1/Plappy-v1/leden.xaml.cs
+++ Plappy-v1/Plappy-v1/leden.xaml.cs
@@ -10,88 +10,74 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Plappy_v1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for leden.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class leden : Window
     {
-        bool toggle = false;
-
-        public MainWindow()
+        public leden()
         {
             InitializeComponent();
         }
 
-        private void Btn_Aanwezig(object sender, RoutedEventArgs e)
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MW = new MainWindow();
+            this.Close();
+            MW.Show();
+        }
+
+        private void Aanwezigheden(object sender, MouseButtonEventArgs e)
         {
             aanwezigheden aanwezigheden = new aanwezigheden();
             this.Close();
             aanwezigheden.Show();
         }
-
-        private void Btn_Leden(object sender, RoutedEventArgs e)
+        private void Leden(object sender, MouseButtonEventArgs e)
         {
             leden leden = new leden();
             this.Close();
             leden.Show();
         }
-
-        private void Btn_Speelmoment(object sender, RoutedEventArgs e)
+        private void Speelmomenten(object sender, MouseButtonEventArgs e)
         {
             speelmomenten speelmomenten = new speelmomenten();
             this.Close();
             speelmomenten.Show();
         }
-
-        private void Btn_sporthal(object sender, RoutedEventArgs e)
+        private void Sporthal(object sender, MouseButtonEventArgs e)
         {
             sporthal sporthal = new sporthal();
             this.Close();
             sporthal.Show();
         }
-
-        private void Btn_Statistieken(object sender, RoutedEventArgs e)
-        {
-            statistieken statistieken = new statistieken();
-            this.Close();
-            statistieken.Show();
-        }
-
-        private void Btn_CoronaAlert(object sender, RoutedEventArgs e)
-        {
-            coronaalert coronaalert = new coronaalert();
-            this.Close();
-            coronaalert.Show();
-        }
-
-        private void Btn_Kalender(object sender, RoutedEventArgs e)
+        private void Kalender(object sender, MouseButtonEventArgs e)
         {
             kalender Kalender = new kalender();
             this.Close();
             Kalender.Show();
         }
-
-        private void Btn_Profiel(object sender, RoutedEventArgs e)
+        private void Statistieken(object sender, MouseButtonEventArgs e)
+        {
+            statistieken statistieken = new statistieken();
+            this.Close();
+            statistieken.Show();
+        }
+        private void CoronaAlert(object sender, MouseButtonEventArgs e)
+        {
+            coronaalert coronaalert = new coronaalert();
+            this.Close();
+            coronaalert.Show();
+        }
+        private void Profiel(object sender, MouseButtonEventArgs e)
         {
             profiel profiel = new profiel();
             this.Close();
             profiel.Show();
         }
-
-        private void Btn_Uitloggen(object sender, RoutedEventArgs e)
-        {
-             login login = new login();
-            this.Close();
-            login.Show();
-
-            //RectUitloggen.Fill = new SolidColorBrush(toggle ? Colors.Aquamarine : Colors.DarkRed);
-            //toggle = !toggle;
-        }
-
     }
 }
